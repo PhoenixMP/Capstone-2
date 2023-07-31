5975975448c07a85f01031aed7d697bba4085be9
PhoenixMP <phoenix.petterson@gmail.com>
2023-07-31 11:47:27 -0700

    updated backend client routes

--- noteworthy-project/backend/app.js
+++ noteworthy-project/backend/app.js
@@ -41,9 +41,9 @@ app.get("/", (req, res) => {
 })
 
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/dist')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
 });
 
 
